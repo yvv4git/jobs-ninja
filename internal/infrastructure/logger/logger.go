2106a7ef72db0e2da95bdf74d5001c3a7014c74b
yvv4git <yvv4m@mail.ru>
2025-03-03 20:31:14 +0300

    Add convert log level from string to logger

--- internal/infrastructure/logger/logger.go
+++ internal/infrastructure/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 )
@@ -15,4 +16,39 @@ func SetupLoggerWithLevel(level slog.Level) *slog.Logger {
 	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: level,
 	}))
+}
+
+type LogLevel string
+
+const (
+	LogLevelDebug LogLevel = "debug"
+	LogLevelInfo  LogLevel = "info"
+	LogLevelWarn  LogLevel = "warn"
+	LogLevelError LogLevel = "error"
+)
+
+func ParseLogLevel(levelStr LogLevel) slog.Level {
+	switch levelStr {
+	case LogLevelDebug:
+		return slog.LevelDebug
+	case LogLevelInfo:
+		return slog.LevelInfo
+	case LogLevelWarn:
+		return slog.LevelWarn
+	case LogLevelError:
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+func (l *LogLevel) UnmarshalText(text []byte) error {
+	level := LogLevel(text)
+	switch level {
+	case LogLevelDebug, LogLevelInfo, LogLevelWarn, LogLevelError:
+		*l = level
+		return nil
+	default:
+		return fmt.Errorf("invalid log level: %s", text)
+	}
 }
