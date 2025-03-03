196c4d7abd0309f846cab054d86c63c8d04206ae
yvv4git <yvv4m@mail.ru>
2025-03-03 15:31:06 +0300

    Add sckeleton

---
+++ cmd/collector/go.mod
@@ -0,0 +1,3 @@
+module github.com/yvv4git/jobs-tg-collector
+
+go 1.23.1
---
+++ internal/infrastructure/clients/telegram.go
@@ -0,0 +1,24 @@
+package clients
+
+import (
+	"context"
+	"log/slog"
+)
+
+type TelegramClient struct {
+	log *slog.Logger
+}
+
+func NewTelegramClient(log *slog.Logger) *TelegramClient {
+	return &TelegramClient{log: log}
+}
+
+func (t *TelegramClient) History(ctx context.Context, source string) error {
+	// TODO: implement
+	return nil
+}
+
+func (t *TelegramClient) Subscribe(ctx context.Context, sources []string) error {
+	// TODO: implement
+	return nil
+}
