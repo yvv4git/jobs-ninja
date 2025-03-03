8d8ef0ab7a96545cf13e34810bcbb41ad45bfddd
yvv4git <yvv4m@mail.ru>
2025-03-03 18:58:04 +0300

    Add entry points

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/yvv4git/jobs-tg-collector/internal/config"
+	"github.com/yvv4git/jobs-tg-collector/internal/infrastructure/logger"
+	"github.com/yvv4git/jobs-tg-collector/internal/service"
+)
+
+const (
+	configPath = "config.toml"
+)
+
+func main() {
+	log := logger.SetupDefaultLogger()
+
+	cfg := config.NewConfig(log)
+	err := cfg.Load(configPath)
+	if err != nil {
+		log.Error("can't load config", "err", err)
+		return
+	}
+
+	fmt.Println("CFG: ", cfg)
+
+	svcCollector := service.NewCollector(log, nil)
+
+	svcCollector.FetchHistory(context.Background(), []string{"@jobs_tg_channel"})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/yvv4git/jobs-tg-collector
 
 go 1.23.1
+
+require (
+	github.com/BurntSushi/toml v1.4.0
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
